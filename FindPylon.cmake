8cf46335240590454c18b1eb7c922a5f635511b8
Gabriel J. Trabasso <gabrieljt@gmail.com>
2014-03-07 22:58:19 -0300

    Indentation

---
+++ FindPylon.cmake
@@ -0,0 +1,27 @@
+set(PYLON_INCLUDE
+    $ENV{PYLON_ROOT}/include/ 
+    $ENV{GENICAM_ROOT_V2_3}/library/CPP/include/
+)
+
+set(PYLON_LIBS
+    $ENV{PYLON_ROOT}/lib64/libpylonbase.so
+    $ENV{PYLON_ROOT}/lib64/libpylonutility.so
+    $ENV{PYLON_ROOT}/lib64/libXerces-C_gcc40_v2_7.so
+    $ENV{GENICAM_ROOT_V2_3}/bin/Linux64_x64/libGCBase_gcc40_v2_3.so
+    $ENV{GENICAM_ROOT_V2_3}/bin/Linux64_x64/libGenApi_gcc40_v2_3.so
+    $ENV{GENICAM_ROOT_V2_3}/bin/Linux64_x64/libMathParser_gcc40_v2_3.so
+    $ENV{GENICAM_ROOT_V2_3}/bin/Linux64_x64/libLog_gcc40_v2_3.so
+    $ENV{GENICAM_ROOT_V2_3}/bin/Linux64_x64/liblog4cpp_gcc40_v2_3.so
+)
+
+# handle the QUIETLY and REQUIRED arguments and set PYLON_FOUND to TRUE if
+# all listed variables are TRUE
+include(${CMAKE_CURRENT_LIST_DIR}/FindPackageHandleStandardArgs.cmake)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(PYLON REQUIRED_VARS PYLON_INCLUDE PYLON_LIBS)
+
+if(PYLON_FOUND)
+  set( PYLON_INCLUDE_DIRS ${PYLON_INCLUDE} )
+  set( PYLON_LIBRARIES ${PYLON_LIBS} )
+endif()
+
+mark_as_advanced(PYLON_INCLUDE PYLON_LIBS)
--- Source/Application.cpp
+++ Source/Application.cpp
@@ -15,9 +15,9 @@ namespace
 {
     // TODO: cross-platform configuration
     const int NUMBER_OF_CAMERAS = 2;
-	const char* CONFIGURATION_FILE = "Config/default_linux.pfs";       // Windows: default.pfs ; Linux: default_linux.pfs
-	const int INTER_PACKET_DELAY = 8192;                               // Windows: 9018 ; Linux: 8192
-	const int FRAME_TRANSMISSION_DELAY = 4096;                         // Windows: 6233 ; Linux: 4096
+    const char* CONFIGURATION_FILE = "Config/default_linux.pfs";       // Windows: default.pfs ; Linux: default_linux.pfs
+    const int INTER_PACKET_DELAY = 8192;                               // Windows: 9018 ; Linux: 8192
+    const int FRAME_TRANSMISSION_DELAY = 4096;                         // Windows: 6233 ; Linux: 4096
     const std::string lineBreak = "================================";    
 }
 
@@ -28,73 +28,73 @@ Application::Application()
 , mDevices()
 , mCameras(NUMBER_OF_CAMERAS)
 {
-	std::cout << "Initializing Stereo Vision..." << std::endl;  
+    std::cout << "Initializing Stereo Vision..." << std::endl;  
 
     // Validations
-	if (mTransportLayerFactory.EnumerateDevices(mDevices) == 0)
-		throw std::runtime_error("Application::Application() - No Camera Devices found");
+    if (mTransportLayerFactory.EnumerateDevices(mDevices) == 0)
+        throw std::runtime_error("Application::Application() - No Camera Devices found");
     assert(mDevices.size() == NUMBER_OF_CAMERAS);
 
-	for (size_t i = 0; i < mDevices.size(); ++i)
-	{	
+    for (size_t i = 0; i < mDevices.size(); ++i)
+    {       
         // Attach device to Pylon's camera array
         mCameras[i].Attach(mTransportLayerFactory.CreateDevice(mDevices[i]));
         Pylon::CInstantCamera &camera = mCameras[i];
-        //Register Configuration and Image Capture Events
+        // Register Configuration and Image Capture Events
         camera.RegisterConfiguration(
             new CameraConfiguration(CONFIGURATION_FILE, INTER_PACKET_DELAY, FRAME_TRANSMISSION_DELAY * (int) (i + 1)), 
             Pylon::RegistrationMode_ReplaceAll, 
             Pylon::Cleanup_Delete
         );
-		// Create OpenCV NamedWindow
-		std::string cameraName("Camera " + std::to_string(i) + ": ");
+                // Create OpenCV NamedWindow
+        std::string cameraName("Camera " + std::to_string(i) + ": ");
         cameraName += camera.GetDeviceInfo().GetModelName();
-		mNamedWindows.push_back(cameraName);
-		cv::namedWindow(cameraName, CV_WINDOW_AUTOSIZE);
-		cv::moveWindow(cameraName, 100 * (i + 1), 100 * (i + 1));
-	}
-    // Triggers Camera Configuration	
+        mNamedWindows.push_back(cameraName);
+        cv::namedWindow(cameraName, CV_WINDOW_AUTOSIZE);
+        cv::moveWindow(cameraName, 100 * (i + 1), 100 * (i + 1));
+    }
+    // Triggers Camera Configuration    
     mCameras.Open();
 }
 
 void Application::run()
-{	
-	if (mCameras.IsOpen())
-	{
+{       
+    if (mCameras.IsOpen())
+    {
         std::cout << lineBreak << std::endl;         
-		std::cout << "Initializing Capture..." << std::endl;
-		capture();
-	}
+        std::cout << "Initializing Capture..." << std::endl;
+        capture();
+    }
     else 
     {
         throw std::runtime_error("Application::run() - Failed to Open Cameras");
     }
-	mCameras.Close();
+    mCameras.Close();
     cv::destroyAllWindows();
 }
 
 void Application::capture()
 {
-	Pylon::CGrabResultPtr grabResult;
-	
-	mCameras.StartGrabbing();	
+    Pylon::CGrabResultPtr grabResult;
+    
+    mCameras.StartGrabbing();       
     std::cout << "Capture started. Press ESC while focused on any window to exit." << std::endl;
-	while(mCameras.IsGrabbing())
-	{
-		mCameras.RetrieveResult(5000, grabResult, Pylon::TimeoutHandling_ThrowException);
-		if (grabResult->GrabSucceeded())
-		{        
+    while(mCameras.IsGrabbing())
+    {
+        mCameras.RetrieveResult(5000, grabResult, Pylon::TimeoutHandling_ThrowException);
+        if (grabResult->GrabSucceeded())
+        {        
             // OpenCV image CV_8U: 8-bits, 1 channel
             auto image = cv::Mat(grabResult->GetHeight(), grabResult->GetWidth(), CV_8UC1, grabResult->GetBuffer());
             // Apply BayerGB8 Filter
             cv::cvtColor(image, image, CV_BayerGB2RGB);
             // Display image
-		    cv::imshow(mNamedWindows[grabResult->GetCameraContext()], image);
-			// Keyboard input break with ESC key
-			int key = cv::waitKey(30);
-			if((key & 255) == 27)
-				break;
-		}
-	}
-	mCameras.StopGrabbing();
+            cv::imshow(mNamedWindows[grabResult->GetCameraContext()], image);
+            // Keyboard input break with ESC key
+            int key = cv::waitKey(30);
+            if((key & 255) == 27)
+                break;
+        }
+    }
+    mCameras.StopGrabbing();
 }
